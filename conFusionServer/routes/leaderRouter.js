85705550ee8dc364e83013f695d02941a7f92a79
gmichalopulos <gregomicha@gmail.com>
2021-02-20 23:30:55 -0300

    Express Generator REST API

---
+++ conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter
+.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res, next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader:' + req.body.name + ' with details ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all the leaders!');
+});
+
+leaderRouter
+.route('/:leaderId')
+.get((req, res, next) => {
+    res.end('Will send details of the leader: ' + req.params.leaderId + ' to you!');
+})
+.post((req, res, next) => {
+    res.end('POST operation not supported on /leaders/:leaderId');
+})
+.put((req, res, next) => {
+    res.write('Updating the leader: ' + req.params.leaderId + ' to you!');
+    res.end('Will update the leader: ' + req.body.name + ' with the details: ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting leader: ' + req.params.leaderId);
+});
+
+module.exports = leaderRouter;
