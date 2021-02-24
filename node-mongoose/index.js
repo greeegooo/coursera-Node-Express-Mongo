9b36b99b843c5b396aaaa0f1d307ba8ccc585df2
gmichalopulos <gregomicha@gmail.com>
2021-02-23 23:50:48 -0300

    Mongoose Part 1

---
+++ node-mongoose/index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect
+.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    var newDish = Dishes({
+        name: 'Uthapizza',
+        description: 'test'
+    })
+
+    newDish
+    .save()
+    .then(dish => {
+        console.log(dish);
+        return Dishes.find({}).exec();
+    })
+    .then(() => mongoose.connection.close())
+    .catch(err => console.log(err));
+});
