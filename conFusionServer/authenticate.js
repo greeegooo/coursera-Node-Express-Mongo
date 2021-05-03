7c545da91c40e5afc9074bf296116c69c49d98ef
gmichalopulos <gregomicha@gmail.com>
2021-05-03 04:17:57 -0300

    Upload files

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -45,7 +45,7 @@ exports.verifyAdmin = function(req, res, next) {
     }
     else {
 
-        var err = new Error('You are not authorized to perform this operation! PUTO');
+        var err = new Error('You are not authorized to perform this operation!');
         err.status = 403;
         return next(err);
     }
